4744dd991156f8ab3e55135a17af720d52020b6b
skliba <kliba.stefano@gmail.com>
2016-04-03 19:58:12 +0200

    registration added, proceeding to splash

--- app/src/main/java/com/feelsgoodman/getinline/activities/LoginActivity.java
+++ app/src/main/java/com/feelsgoodman/getinline/activities/LoginActivity.java
@@ -2,9 +2,11 @@ package com.feelsgoodman.getinline.activities;
 
 import com.feelsgoodman.getinline.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by itepavac on 03/04/16.
@@ -14,7 +16,18 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_register)
+    public void registerClicked() {
+        startActivity(new Intent(this, RegistrationActivity.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
     }
 }
