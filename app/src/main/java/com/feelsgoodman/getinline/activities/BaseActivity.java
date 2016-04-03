51b1dc8bad6e581fb53fb582236fa07f540e4820
Igor Tepavac <igor.tepavac@infinum.hr>
2016-04-03 15:34:22 +0200

    Implement basic login layout

---
+++ app/src/main/java/com/feelsgoodman/getinline/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.feelsgoodman.getinline.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by itepavac on 03/04/16.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/feelsgoodman/getinline/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.feelsgoodman.getinline.activities;
+
+import com.feelsgoodman.getinline.R;
+
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by itepavac on 03/04/16.
+ */
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
