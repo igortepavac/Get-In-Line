f3c1351ebd35c8b8de8a0d047aff371782f80b78
Igor Tepavac <igor.tepavac@infinum.hr>
2016-04-03 14:11:34 +0200

    Setup basic project structure

---
+++ app/src/main/java/com/feelsgoodman/getinline/GetInLineApplication.java
@@ -0,0 +1,10 @@
+package com.feelsgoodman.getinline;
+
+import android.app.Application;
+
+/**
+ * Created by itepavac on 03/04/16.
+ */
+public class GetInLineApplication extends Application {
+
+}
